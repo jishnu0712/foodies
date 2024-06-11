b4159d81d0242cca7c20f775d16c535bc538fcb5
jishnu <jishnu.dev.roy@gmail.com>
2024-06-11 17:59:54 +0530

    navlink component setup

--- components/main-header/main-header.jsx
+++ components/main-header/main-header.jsx
@@ -1,31 +1,37 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 import logoImg from "@/assets/logo.png";
 
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+  const path = usePathname();
+
   return (
     <>
-        <MainHeaderBackground/>
-        <header className={classes.header}>
+      <MainHeaderBackground />
+      <header className={classes.header}>
         <Link href="/" className={classes.logo}>
-            <Image src={logoImg} alt="A plate with food on it" priority />
-            Next level food
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          Next level food
         </Link>
         <nav className={classes.nav}>
-            <ul>
+          <ul>
             <li>
-                <Link href="/meals">Browse meals</Link>
+              <NavLink href="/meals">Best meals</NavLink>
             </li>
             <li>
-                <Link href="/community">Foodies community</Link>
+              <NavLink href="/community">Foodies community</NavLink>
             </li>
-            </ul>
+          </ul>
         </nav>
-        </header>
+      </header>
     </>
   );
 }
